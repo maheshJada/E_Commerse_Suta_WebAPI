2fd7c5540405feb19bd13b313c4b256b567791b1
Mahesh Jada <mjada@innominds.com>
2021-11-25 10:19:34 +0530

    completed apis

--- E_Commerse_Suta_WebAPI/Program.cs
+++ E_Commerse_Suta_WebAPI/Program.cs
@@ -15,7 +15,6 @@ namespace E_Commerse_Suta_WebAPI
         {
             CreateHostBuilder(args).Build().Run();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
